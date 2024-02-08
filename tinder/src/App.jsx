4668b74c60f0ac311c3cfc33be73db7eed9f61f3
Himanshu <hgusain088@gmail.com>
2024-02-08 11:13:48 +0530

    create pages and routing

--- tinder/src/App.jsx
+++ tinder/src/App.jsx
@@ -1,5 +1,17 @@
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import Onboarding from "./pages/Onboarding"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
-  return <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<Onboarding />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
