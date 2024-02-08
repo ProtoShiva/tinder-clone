9f2ee85a277ececcfe42cf92c010c6a28dbd0eb4
Himanshu <hgusain088@gmail.com>
2024-02-08 15:03:36 +0530

    create login/signup ui

--- tinder/src/components/AuthModal.jsx
+++ tinder/src/components/AuthModal.jsx
@@ -1,16 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faXmark } from "@fortawesome/free-solid-svg-icons"
 
 const AuthModal = ({ setShowModal }) => {
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [confirmPassword, setConfirmPassword] = useState(null)
+  const [error, setError] = useState(null)
+  const isSignUp = true
   const handleClick = () => {
     setShowModal(false)
   }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <div className="auth-modal">
-      <div onClick={handleClick}>
+      <div className="close-icon" onClick={handleClick}>
         <FontAwesomeIcon icon={faXmark} />
       </div>
+      <h2>{isSignUp ? "Create Account" : "Log in"}</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password-check">Confirm Password</label>
+        <input
+          type="password"
+          id="password-check"
+          name="password-check"
+          placeholder="confirm Password"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input className="secondary-button" type="submit" />
+        <p>{error}</p>
+        <hr />
+        <h2>Get the App</h2>
+      </form>
     </div>
   )
 }
