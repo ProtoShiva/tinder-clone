0497fdd5542a47873b5386024c88c311f0da8d0e
Himanshu <hgusain088@gmail.com>
2024-02-08 11:50:53 +0530

    create Nav bar logo

---
+++ tinder/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import whiteLogo from "../images/tinder_logo_white.png"
+import colorLogo from "../images/color-logo-tinder.png"
+
+const Nav = ({ minimal }) => {
+  return (
+    <nav>
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={minimal ? colorLogo : whiteLogo}
+          alt="tinder"
+        />
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
--- tinder/src/pages/Home.jsx
+++ tinder/src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react"
-
+import Nav from "../components/Nav"
 const Home = () => {
-  return <div>Home</div>
+  const authToken = false
+  const handleClick = () => {
+    console.log("clicked button")
+  }
+  return (
+    <>
+      <Nav minimal={false} />
+      <div className="home">
+        <h1>Start something epic.</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "SignIn" : "Create Acccount"}
+        </button>
+      </div>
+    </>
+  )
 }
 
 export default Home
